2d5db0fb7b40eb00d4d9ed4afc2283ff0198ae8f
atrox3d <atrox3d@gmail.com>
2023-11-05 07:43:00 +0100

    set framerate

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ screen = pygame.display.set_mode((width, height))       # create display surface
 game_title = 'Runner'
 pygame.display.set_caption(game_title)                  # set window title
 
+clock = pygame.time.Clock()                             # instantiate clock object
+
 while True:                                             # main loop
     for event in pygame.event.get():                    # loop through events
         if event.type == pygame.QUIT:                   # quit by closing window
@@ -19,4 +21,6 @@ while True:                                             # main loop
     # draw all our elements
     # update everything
     pygame.display.update()                             # update display surface
+    clock.tick(60)                                      # set framerate: 60fps/one loop every 1.666 milliseconds
+
 
