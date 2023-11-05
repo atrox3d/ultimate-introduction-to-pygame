67e891547a406b0476647509e13dc4501904f1de
atrox3d <atrox3d@gmail.com>
2023-11-05 06:59:39 +0100

    create main loop

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import pygame
+from sys import exit
+
+pygame.init()                                           # initialize engine
+
+width = 800
+height = 400
+screen = pygame.display.set_mode((width, height))       # create display surface
+
+while True:                                             # main loop
+    for event in pygame.event.get():                    # loop through events
+        if event.type == pygame.QUIT:                   # quit by closing window
+            pygame.quit()
+            exit()                                      # prevent further updates
+    # draw all our elements
+    # update everything
+    pygame.display.update()                             # update display surface
+
