707ccda73c4e9831ab66736ae7979dc6fc7ca784
atrox3d <atrox3d@gmail.com>
2023-11-13 10:24:24 +0100

    creating obstacles sprites...

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from pathlib import Path
 
 import pygame
@@ -52,6 +53,43 @@ class Player(pygame.sprite.Sprite):
         self.input()
         self.apply_gravity()
         self.animation()
+
+
+class Obstacle(pygame.sprite.Sprite):
+    FLY = 'fly'.lower()
+    SNAIL = 'snail'.lower()
+
+    FLY_IMAGES_PATH = Path('graphics/Fly')
+    FLY1 = FLY_IMAGES_PATH / 'Fly1'
+    FLY2 = FLY_IMAGES_PATH / 'Fly2'
+
+    SNAIL_IMAGES_PATH = Path('graphics/snail')
+    SNAIL1 = SNAIL_IMAGES_PATH / 'snail1'
+    SNAIL2 = SNAIL_IMAGES_PATH / 'snail2'
+
+    def __init__(self, type):
+        super().__init__()
+
+        self.type = type.lower()
+        self.y_pos = 0
+
+        if self.type == self.FLY:
+            fly1 = pygame.image.load(self.FLY1).convert_alpha()
+            fly2 = pygame.image.load(self.FLY2).convert_alpha()
+            self.frames = [fly1, fly2]
+            self.y_pos = 210
+        elif self.type == self.SNAIL:
+            snail1 = pygame.image.load(self.SNAIL1).convert_alpha()
+            snail2 = pygame.image.load(self.SNAIL2).convert_alpha()
+            self.frames = [snail1, snail2]
+            self.y_pos = 300
+        else:
+            raise TypeError(f'Unknown obstacle type: {self.type}')
+
+        self.index = 0
+        self.image = self.frames[self.index]
+        self.rect = self.image.get_rect(midbottom=random.randint(900, 1100), self.y_pos)
+
 
 def display_score():
     """
