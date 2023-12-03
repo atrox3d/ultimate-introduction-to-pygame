78da5bfa2ca790159ecb8e8d6d9a9caf233df1ba
atrox3d <atrox3d@gmail.com>
2023-12-03 08:27:40 +0100

    reorganize player's properties in categories with Simplenamespace

--- sprites/player.py
+++ sprites/player.py
@@ -1,17 +1,19 @@
 from pathlib import Path
+from types import SimpleNamespace
 
 import pygame
 
 
 class Player(pygame.sprite.Sprite):
-    IMAGES_PATH = Path('graphics/player')
-    WALK1 = IMAGES_PATH / 'player_walk_1.png'
-    WALK2 = IMAGES_PATH / 'player_walk_2.png'
-    JUMP = IMAGES_PATH / 'jump.png'
-    STAND = IMAGES_PATH / 'player_stand.png'
+    paths = SimpleNamespace()
+    paths.IMAGES_PATH = Path('graphics/player')
+    paths.WALK1 = paths.IMAGES_PATH / 'player_walk_1.png'
+    paths.WALK2 = paths.IMAGES_PATH / 'player_walk_2.png'
+    paths.JUMP = paths.IMAGES_PATH / 'jump.png'
+    paths.STAND = paths.IMAGES_PATH / 'player_stand.png'
 
-    SOUNDS_PATH = Path('audio')
-    JUMP_SOUND = SOUNDS_PATH / 'jump.mp3'
+    paths.SOUNDS_PATH = Path('audio')
+    paths.JUMP_SOUND = paths.SOUNDS_PATH / 'jump.mp3'
 
     LEFT = 80
     BOTTOM = 300
@@ -20,40 +22,45 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
 
-        walk1 = pygame.image.load(self.WALK1).convert_alpha()  # load player and convert respecting alpha channel
-        walk2 = pygame.image.load(self.WALK2).convert_alpha()  # load player and convert respecting alpha channel
-        self.walk = [walk1, walk2]
+        self.images = SimpleNamespace()
+
+        self.images.walk1 = pygame.image.load(self.paths.WALK1).convert_alpha()  # load player and convert respecting alpha channel
+        self.images.walk2 = pygame.image.load(self.paths.WALK2).convert_alpha()  # load player and convert respecting alpha channel
+        self.images.walk = [self.images.walk1, self.images.walk2]
         self.index = 0
-        self.jump = pygame.image.load(self.JUMP).convert_alpha()  # load player and convert respecting alpha channel
+        self.images.jump = pygame.image.load(self.paths.JUMP).convert_alpha()  # load player and convert respecting alpha channel
 
-        self.stand = pygame.image.load(self.STAND).convert_alpha()  # load player and convert respecting alpha channel
+        self.images.stand = pygame.image.load(self.paths.STAND).convert_alpha()  # load player and convert respecting alpha channel
 
-        self.image = self.walk[self.index]
+        self.image = self.images.walk[self.index]
         self.rect = self.image.get_rect(midbottom=Player.MIDBOTTOM)
         self.gravity = 0
 
-        self.jump_sound = pygame.mixer.Sound(self.JUMP_SOUND)
-        self.jump_sound.set_volume(0.5)
+        self.sounds = SimpleNamespace()
+        self.sounds.jump = pygame.mixer.Sound(self.paths.JUMP_SOUND)
+        self.sounds.jump.set_volume(0.5)
 
     def input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE] and self.rect.bottom >= Player.BOTTOM:
-            # TODO: create Player.jump()
-            self.gravity = -20
-            self.jump_sound.play()
+            self.jump()
+
+    def jump(self):
+        self.gravity = -20
+        self.sounds.jump.play()
 
     def get_player_stand(self):
-        stand = pygame.transform.rotozoom(self.stand, 0, 2)  # scale by 2 at angle 0
+        stand = pygame.transform.rotozoom(self.images.stand, 0, 2)  # scale by 2 at angle 0
         stand_rect = stand.get_rect(center=(400, 200))  # get centered rect
         return stand, stand_rect
 
     def animation(self):
         if self.rect.bottom < self.BOTTOM:
-            self.image = self.jump
+            self.image = self.images.jump
         else:
             self.index += 0.1
-            if self.index >= len(self.walk): self.index = 0
-            self.image = self.walk[int(self.index)]
+            if self.index >= len(self.images.walk): self.index = 0
+            self.image = self.images.walk[int(self.index)]
 
     def apply_gravity(self):
         self.gravity += 1
